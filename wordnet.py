9d49a0094da9afa92e48e7cbd1c6b4ff9c8d5610
ng700756 <ng700756@iteso.mx>
2017-08-08 21:13:44 -0500

    python version

---
+++ wordnet.py
@@ -0,0 +1,32 @@
+import nltk
+from nltk.tokenize import sent_tokenize
+from nltk.tokenize import word_tokenize
+from nltk.tokenize import RegexpTokenizer
+from nltk.tokenize import PunktSentenceTokenizer
+from nltk.corpus import stopwords
+from nltk.corpus import webtext
+from nltk.corpus import wordnet 
+from flask import Flask
+app = Flask(__name__)
+
+nltk.download('omw') # Open Multilingual WordNet
+from nltk.corpus import wordnet as wn
+print(wn.lemmas('prueba', lang='spa'))
+
+@app.route('/lemmas/<string:word1>')
+def index(word1):
+	word = word1 
+	result = str(wn.lemmas(word, lang='spa'))
+	print("[][]",word,result)
+	return result 
+@app.route('/synsets/<string:word1>')
+def index3(word1):
+	word = word1
+	syn = wordnet.synsets(word, lang='spa')
+	s = ""
+	for w in syn:
+		s += "{} - {}\n{}\n".format(w.name(), w.definition(), w.hypernyms())	
+	return s 
+
+if __name__ == '__main__':
+    app.run(port=5004)
